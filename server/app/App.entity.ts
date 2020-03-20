47c4529683aae934eab819f027d714c0a4ea6374
maorRoz <andory@post.bgu.ac.il>
2020-03-20 19:56:38 +0200

    init2

--- server/app/App.entity.ts
+++ server/app/App.entity.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, OneToMany, ObjectType } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
+// eslint-disable-next-line import/no-cycle
 import { ReleaseNotes } from '../release-notes';
 
 @Entity()
